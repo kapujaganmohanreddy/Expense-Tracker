40783f9bee8c909dd3a254be57fd4a6adc595a89
Kapu Jaganmohan Reddy <kapujaganmohanreddybsp@gmail.com>
2025-11-08 23:40:15 +0530

    Updated for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -33,4 +33,5 @@ app.use("/api/v1/expense",expenseRoutes);
 app.use("/api/v1/dashboard",dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+// app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+module.exports = app;
